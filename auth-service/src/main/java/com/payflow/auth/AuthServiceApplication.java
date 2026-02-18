cdcef65a20380fc5137f42c15532fedf85105612
Gabriel Marisescu <gabriel.marisescu@dxc.com>
2026-02-18 17:41:02 +0100

    fix: postprocess application.yml and update main class name

--- auth-service/src/main/java/com/payflow/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/payflow/auth/AuthServiceApplication.java
@@ -1,13 +1,14 @@
-package com.payflow.auth_service;
+package com.payflow.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
